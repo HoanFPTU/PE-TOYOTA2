94ef4a2bf5e061c26d243d2145ca7d859cdda81c
HoanFPTU <nguyenhoan20032003@gmail.com>
2023-11-30 23:04:45 +0700

    first commit

---
+++ Resource/js/mycode.js
@@ -0,0 +1,7 @@
+const mySidenav = document.getElementById("mySidenav");
+const openNav = () => {
+  mySidenav.style.width = "400px";
+};
+const closeNav = () => {
+  mySidenav.style.width = "0px";
+};
